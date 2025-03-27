6390a49e538ec096648725c3fb55383c7408dd3f
William-Ro <william.r2409@gmail.com>
2025-03-27 08:59:39 -0600

    chore: fix deprecated declaration in employee entity

--- pkg/entities/employee.go
+++ pkg/entities/employee.go
@@ -3,20 +3,20 @@ package entities
 import (
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 // Employee Constructs your Employee model under entities.
 type Employee struct {
-	ID         primitive.ObjectID `json:"id"  bson:"_id,omitempty"`
-	First_Name string             `json:"first_name" bson:"first_name"`
-	Last_Name  string             `json:"last_name" bson:"last_name"`
-	Email      string             `json:"email" bson:"email"`
-	CreatedAt  time.Time          `json:"createdAt" bson:"createdAt"`
-	UpdatedAt  time.Time          `json:"updatedAt" bson:"updatedAt"`
+	ID         bson.ObjectID `json:"id"  bson:"_id,omitempty"`
+	First_Name string        `json:"first_name" bson:"first_name"`
+	Last_Name  string        `json:"last_name" bson:"last_name"`
+	Email      string        `json:"email" bson:"email"`
+	CreatedAt  time.Time     `json:"createdAt" bson:"createdAt"`
+	UpdatedAt  time.Time     `json:"updatedAt" bson:"updatedAt"`
 }
 
-// DeleteRequest struct is used to parse Delete Requests for Employee
+// DeleteRequest struct is used to parse Delete Requests for Employees
 type DeleteRequest struct {
 	ID string `json:"id"`
 }
